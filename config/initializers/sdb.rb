8bbd1a30278d546c67d0651a32ef9363fcb03dfc
Mike Yang <yfractal@gmail.com>
2025-02-18 22:50:21 +0800

    feat: setup sdb_signal

--- config/initializers/sdb.rb
+++ config/initializers/sdb.rb
@@ -4,3 +4,12 @@ require 'puma'
 GC.auto_compact = true
 
 Sdb::PumaPatch.patch(Rails.logger)
+
+SdbSignal.setup_signal_handler
+
+Thread.new do
+  sleep 5 # wait for puma threads start
+  threads = Thread.list.select { |thread| thread != Thread.current }
+  SdbSignal.start_scheduler(threads)
+  puts "SdbSignal scheduler started to scan threads=#{threads}"
+end
