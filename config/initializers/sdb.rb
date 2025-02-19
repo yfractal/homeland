80f474e657aaa19053c43ebe17a129e702008f3b
Mike Yang <yfractal@gmail.com>
2025-02-19 09:04:32 +0800

    feat: setup sdb

--- config/initializers/sdb.rb
+++ config/initializers/sdb.rb
@@ -4,3 +4,16 @@ require 'puma'
 GC.auto_compact = true
 
 Sdb::PumaPatch.patch(Rails.logger)
+
+Thread.new do
+  sleep 5
+
+  threads = Thread.list
+
+  puts "threads.count: #{threads.count}"
+  threads.each do |thread|
+    puts "[#{thread.native_thread_id}] #{thread.name}"
+  end
+
+  Sdb.scan_puma_threads(0.001)
+end
