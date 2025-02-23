90c03f09f66e349cc4bbcb97f0dbc91780dfcbe5
Mike Yang <yfractal@gmail.com>
2025-02-23 10:46:26 +0800

    feat: trigger gc comact

--- config/initializers/sdb.rb
+++ config/initializers/sdb.rb
@@ -11,9 +11,23 @@ Thread.new do
   threads = Thread.list
 
   puts "threads.count: #{threads.count}"
+  puma_threads = []
   threads.each do |thread|
-    puts "[#{thread.native_thread_id}] #{thread.name}"
+    if thread.name&.include?('puma srv tp')
+      puma_threads << thread
+      puts "[#{thread.native_thread_id}] #{thread.name}"
+    end
+    #puts "[#{thread.native_thread_id}] #{thread.name}"
   end
+
+  Sdb.scan_threads(puma_threads, 0.001)
 end
 
-Sdb.scan_all_threads(0.001)
+Thread.new {
+  loop {
+    sleep 3
+    puts "gc compacted"
+    GC.compact
+  }
+
+}
