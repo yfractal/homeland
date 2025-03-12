8b592c0e0cdc7a747a380640bb85987425c029e7
Mike Yang <yfractal@gmail.com>
2025-03-12 23:20:53 +0800

    feat: us new method

--- config/initializers/sdb.rb
+++ config/initializers/sdb.rb
@@ -5,21 +5,4 @@ puts 'Enable auto compact'
 GC.auto_compact = true
 
 Sdb::PumaPatch.patch(Rails.logger)
-
-Thread.new do
-  sleep 5
-
-  threads = Thread.list
-
-  puts "threads.count: #{threads.count}"
-  puma_threads = []
-  threads.each do |thread|
-    if thread.name&.include?('puma srv tp')
-      puma_threads << thread
-      puts "[#{thread.native_thread_id}] #{thread.name}"
-    end
-    #puts "[#{thread.native_thread_id}] #{thread.name}"
-  end
-
-  Sdb.scan_threads(puma_threads, 0.001)
-end
+Sdb.scan_puma_threads(0.001)
